13e742f978e375abc09cb1e81069758d37c6f33e
TuNombre <garciahernandezjoseph9@gmail.com>
2025-10-02 23:53:40 -0600

    Cuarto commit

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -2,9 +2,15 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js'  
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express()
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
 
 app.use(express.json())
 
